fff71cfbb990ce4c3718d8b58de63bb6c1f0cab1
David Kim <hhjj0506@gmail.com>
2015-03-24 14:59:28 -0700

    3/24/15

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -43,3 +43,33 @@ game.HeroDeathManager = Object.extend({
     }
 });
 
+game.ExperienceManager = Object.extend({
+    init: function() {
+        this.alwaysUpdate = true;
+        this.gameOver = false;
+    },
+    
+    update: function() {
+        if(game.data.win === true && !this.gameOver){
+            this.gameOver(true);
+        }else if(game.data.win === false && !this.gameOver){
+            this.gameOver(false);
+        }
+        
+        return true;
+    },
+    
+    gameOver: function(win) {
+        if(win){
+            game.data.exp += 10;
+        }else{
+            game.data.exp += 1;
+        }
+       
+        this.gameOver = true;
+        me.save.exp = game.data.exp;
+        //for testing purposes only
+        me.save.exp2 = 4;
+    }
+});
+
