b425cec742f4c0d67572467de1c76c80776abfab
David Kim <hhjj0506@gmail.com>
2015-03-25 15:00:16 -0700

    3/25/15

---
+++ js/screens/spendExp.js
@@ -0,0 +1,32 @@
+game.SpendExp = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+            me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('exp-screen')), -10); // TODO
+            
+            me.game.world.addChild(new (me.Renderable.extend({
+                init: function() {
+                    this._super(me.Renderable, 'init', [10, 10, 300, 50]);
+                    this.font = new me.Font("Arial", 46, "white");
+                },
+                
+                draw: function(renderer) {
+                    this.font.draw(renderer.getContext(), "PRESS F1-F4 TO BUY, F5 TO SKIP", this.pos.x, this.pos.y);
+                    this.font.draw(renderer.getContext(), "THY EXP: " + game.data.exp.toString(), this.pos.x + 100, this.pos.y + 50);
+                    this.font.draw(renderer.getContext(), "F1: GOLD DIGGER " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 100);
+                    this.font.draw(renderer.getContext(), "F2: RICH KID " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 150);
+                    this.font.draw(renderer.getContext(), "F3: WORKOUT " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 200);
+                    this.font.draw(renderer.getContext(), "F4: STEROID " + game.data.exp.toString(), this.pos.x + 200, this.pos.y + 250);
+                }
+            })));
+        },
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+
+	}
+});
